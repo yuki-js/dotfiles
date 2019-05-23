bc8223e7e9c1f9973ac183b139717bc594bfa847
anonymous <>
2019-05-24 01:51:11 +0900

    show hidden

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# ask
+
+read -p "Continue (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+    exit
+fi
+
+echo "Installing..."
+
+
 # install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install emacs
 brew install zsh
 
+# symlink dotfiles
 DOT_FILES=(.emacs.d .zshrc .zprofile)
 
 for file in ${DOT_FILES[@]}
@@ -14,3 +25,10 @@ do
 done
 
 chsh -s /usr/local/bin/zsh
+
+# show hidden files
+
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+
+echo "Finished!"

9d688b814d74aa75cbd7cc752004813173d34811
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2019-05-24 13:12:37 +0900

    Worked perfectly

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 # ask
 
+echo "Check if this file is $HOME/codes/dotfiles/install.sh"
+
 read -p "Continue (y/n)?" CONT
 if [ "$CONT" != "y" ]; then
     exit
@@ -11,9 +13,9 @@ echo "Installing..."
 
 
 # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install emacs
+brew cask install emacs
 brew install zsh
 brew install git
 
@@ -22,15 +24,21 @@ DOT_FILES=(.emacs.d .zshrc .zprofile)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s ./$file $HOME/$file
+    if [ -e "$HOME/codes/dotfiles/$file" ] ; then
+	ln -s $HOME/codes/dotfiles/$file $HOME/$file
+    else
+	echo "File $HOME/codes/dotfiles/$file not found"
+    fi
 done
 
+sudo bash -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 chsh -s /usr/local/bin/zsh
 
 # show hidden files
 
 defaults write com.apple.Finder AppleShowAllFiles true
 killall Finder
+
 
 echo "Finished! Opening Emacs... Press C-x C-c later."
 sleep 3

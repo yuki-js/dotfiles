6b89ebd1338e9cee79a79b2c33aa3d23ba37d78d
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2019-09-26 16:03:38 +0900

    replaced python environment to elpy

--- .emacs.d/inits/15-python.el
+++ .emacs.d/inits/15-python.el
@@ -1,28 +1,21 @@
-(el-get-bundle python)
-(el-get-bundle py-yapf)
-(el-get-bundle jedi)
-
-(add-to-list 'auto-mode-alist '("\\\.py\\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
-
-(defun tnoda/turn-on-flycheck-mode ()
-  (flycheck-mode 1))
-(add-hook 'python-mode-hook 'tnoda/turn-on-flycheck-mode)
-(add-hook 'python-mode-hook 'jedi:setup)
-(setq jedi:complete-on-dot t)
-
-(add-hook 'python-mode-hook 'py-yapf-enable-on-save)
-
-(setq python-indent-offset 4)
+(el-get-bundle elpy)
+(elpy-enable)
+(setq elpy-rpc-python-command "python3")
+(elpy-use-ipython "python3")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flycheck-python-flake8-executable nil)
- '(py-python-command "python3")
  '(flycheck-python-flake8-executable "python3")
  '(flycheck-python-pycompile-executable "python3")
  '(flycheck-python-pylint-executable "python3")
-)
+ '(py-python-command "python3")
+ '(python-shell-interpreter "python3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

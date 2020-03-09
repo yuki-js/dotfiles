e14645b6daa7023a956adee28fd17c4e47d4f749
yuki-js <20838151+yuki-js@users.noreply.github.com>
2020-03-10 05:35:46 +0900

    add magit & C-R keybind for replace-string

--- .emacs.d/inits/00-keybind.el
+++ .emacs.d/inits/00-keybind.el
@@ -1,3 +1,4 @@
 ;; C-h to delete a character
 (keyboard-translate ?\C-h ?\C-?)
 (global-set-key (kbd "C-x C-k") 'kill-this-buffer)
+(global-set-key (kbd "C-R") 'replace-string)
---
+++ .emacs.d/inits/05-magit.el
@@ -0,0 +1,2 @@
+(el-get-bundle magit)
+(global-set-key (kbd "C-x g") 'magit-status)

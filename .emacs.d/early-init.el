3f368899f488d52ba99d363d37651302437d91db
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2020-08-12 16:33:19 +0900

    disable package on startup

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -1,8 +1,3 @@
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-
+(setq package-enable-at-startup nil)
+(setq default-directory "~/")
+(setq command-line-default-directory "~/")
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,11 @@
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+
 (el-get-bundle emacs-jp/init-loader)
 
 ;; bytecompile init-loader

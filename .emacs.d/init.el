6152374f2b3f90a6bba38a39457424989a4ad78f
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2020-10-01 17:26:06 +0900

    refine js mode!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(svelte-mode dashboard vue-mode vue-html-mode ssass-mode org rustic racer company)))
+   '(auto-complete tern-auto-complete svelte-mode dashboard vue-mode vue-html-mode ssass-mode org rustic racer company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

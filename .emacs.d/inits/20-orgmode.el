0fab9531f5f8677606ac337ac2dd36b7ae558848
Yuki Aoki <20838151+yuki-js@users.noreply.github.com>
2020-11-25 17:12:04 +0900

    jupyter

--- .emacs.d/inits/20-orgmode.el
+++ .emacs.d/inits/20-orgmode.el
@@ -2,3 +2,10 @@
 
 (setq org-latex-create-formula-image-program 'dvipng)
 (setq org-html-validation-link nil)
+(setq org-confirm-babel-evaluate nil)
+(el-get-bundle ob-ipython)
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((ipython . t)
+   ;; other languages..
+   ))

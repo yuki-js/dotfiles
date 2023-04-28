349d142de0dd181760dab3023625cbcfe7a7f0f0
yuki-js <me@aoki.app>
2023-04-29 01:43:15 +0900

    add wsl config

---
+++ scripts/debian-like/wsl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+
+# abort if superuser
+if [ $(id -u) -eq 0 ]; then
+  echo "Fatal error: This script must not be run as root"
+  exit 1
+fi
+MAKE_ME_ROOT="sudo"
+# assume debian like distro, no check
+
+# if not wsl2, pass
+if [ ! -f /proc/sys/fs/binfmt_misc/WSLInterop ]; then
+  echo "Not WSL2, skipping"
+  exit 0
+fi
+
+# cp /etc/wsl.conf
+$MAKE_ME_ROOT cp -f /etc/wsl.conf /etc/wsl.conf.bak
+$MAKE_ME_ROOT cp -f $HOME/codes/dotfiles/cfg/wsl.conf /etc/wsl.conf

04aaaeb84a1c1525330b5cdcea845101abc7c210
yuki-js <me@aoki.app>
2024-11-18 02:57:53 +0900

    change the way global git ignore file is handled

--- scripts/debian-like/shell-experience.sh
+++ scripts/debian-like/shell-experience.sh
@@ -11,7 +11,6 @@ MAKE_ME_ROOT="sudo"
 # assume debian like distro, no check
 
 FILES_TO_BE_TRANSPLANTED_TO_HOME=(
-  $HOME/codes/dotfiles/cfgs/.gitignore_global
   $HOME/codes/dotfiles/cfgs/.zshrc
 )
 
@@ -23,9 +22,20 @@ for file in ${FILES_TO_BE_TRANSPLANTED_TO_HOME[@]}; do
   echo "Made a symbolic link ($file -> $HOME)"
 done
 
-git config --global core.excludesfile $HOME/.gitignore_global
 # set zsh as default shell
 chsh -s $(which zsh)
 
+# git configs
 git config --global user.email "me@aoki.app"
 git config --global user.name "yuki-js"
+
+# if XDG_CONFIG_HOME is set:
+if [ -n "${XDG_CONFIG_HOME:-}" ]; then
+  # create a symbolic link to $XDG_CONFIG_HOME
+  mkdir -p $XDG_CONFIG_HOME/git
+  ln -sf $HOME/codes/dotfiles/cfgs/.gitignore_global $XDG_CONFIG_HOME/git/ignore
+else
+  # create a symbolic link to $HOME
+  mkdir -p $HOME/.config/git
+  ln -sf $HOME/codes/dotfiles/cfgs/.gitignore_global $HOME/.config/git/ignore
+fi
